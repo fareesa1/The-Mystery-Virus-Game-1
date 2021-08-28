54316159f584dfd90e6f10c13579224b69bcd8ee
OmarThe <stomatoth@gmail.com>
2021-08-28 14:17:26 -0400

    Second Update

--- sketch.js
+++ sketch.js
@@ -33,11 +33,11 @@ function preload(){
   
 
   player_img= loadAnimation("flatboy/png/Idle (1).png","flatboy/png/Idle (2).png", "flatboy/png/Idle (3).png", "flatboy/png/Idle (4).png",
-     "flatboy/png/Idle (5).png", "flatboy/png/Idle (6).png","flatboy/png/Idle (7).png", "flatboy/png/Idle (8).png", "flatboy/png/Idle (9).png",
+     "flatboy/png/Idle(5).png", "flatboy/png/Idle (6).png","flatboy/png/Idle (7).png", "flatboy/png/Idle (8).png", "flatboy/png/Idle (9).png",
      "flatboy/png/Idle (10).png", "flatboy/png/Idle (11).png", "flatboy/png/Idle (12).png", "flatboy/png/Idle (13).png", "flatboy/png/Idle (14).png",
      "flatboy/png/Idle (15).png")
 
-  player_Dead_img= loadAnimation("flatboy/png/dead (1).png","flatboy/png/dead (2).png", "flatboy/png/dead (3).png", "flatboy/png/dead (4).png",
+  player_Dead_img= loadAnimation("flatboy/png/dead(1).png","flatboy/png/dead(2).png", "flatboy/png/dead(3).png", "flatboy/png/dead(4).png",
      "flatboy/png/dead (5).png", "flatboy/png/dead (6).png","flatboy/png/dead (7).png", "flatboy/png/dead (8).png", "flatboy/png/dead (9).png",
      "flatboy/png/dead (10).png", "flatboy/png/dead (11).png", "flatboy/png/dead (12).png", "flatboy/png/dead (13).png", "flatboy/png/Idle (14).png",
      "flatboy/png/Idle (15).png")
